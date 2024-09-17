65fba17d97fc5f5b9bc8ec484378b94ad69d3ef4
Amanda Mensah-Biney <airlinepilotamanda@gmail.com>
2024-09-17 15:55:28 +0000

    updated changes

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,22 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector('#register'); 
 //Handle form submit event.
-registerForm.onsubmit = function(event) {
+registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
-console.log(event);
+    //collect form data
+    const formData = new FormData(registerForm);
+    //send data to backend
+    //display success message to user 
+    const messageH1 = document.querySelector('#message');
+    messageH1.textContent = 'Account registered successfully!';
+});
+
+//declare variable age
+var age = 71;
+//define square age function
+function squareAge (age) {
+    return age ** 2;
 }
+
+const squaredAge = squareAge(34);
+console.log(squaredAge);
